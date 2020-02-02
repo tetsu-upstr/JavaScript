a780219d3a3442c96802fb154d02211e6752d778
tetsu-upstr <bodomu@gmail.com>
2020-02-02 12:30:30 +0900

    要素生成、ボタンクリック関数

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function displayMessage() {
+ var html = document.querySelector('html');
+
+ var panel = document.createElement('div');
+ panel.setAttribute('class', 'msgBox');
+ html.appendChild(panel);
+
+ var msg = document.createElement('p');
+ msg.textContent = 'This is a message box';
+ panel.appendChild(msg);
+
+ var closeBtn = document.createElement('button');
+ closeBtn.textContent = 'x';
+ panel.appendChild(closeBtn);
+
+ closeBtn.onclick = function() {
+   panel.parentNode.removeChild(panel);
+ } 
+}
+
+var btn = document.querySelector('button');
+// displayMessage()とすると、ブラウザリロードで関数が実行されてしまう
+btn.onclick = displayMessage;
